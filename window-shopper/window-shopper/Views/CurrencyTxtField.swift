cad627e7bfa94fe36f20db3f0f4f22045579f23b
Carlos Cuba <carlos.cuba@bonzzu.com>
2017-08-14 00:33:22 -0500

    Text fields added and configured. Calculate button programatically created and added as accesory view to the keyboard when text fields are focused.

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
 
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+
     override func awakeFromNib() {
-        super.awakeFromNib()
-        
+        customizeView()
+    }
+    
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2468635949)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -23,5 +30,4 @@ class CurrencyTxtField: UITextField {
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
-
 }
