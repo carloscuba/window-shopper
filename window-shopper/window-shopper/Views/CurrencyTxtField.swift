fbeec69981c6d1cb5d3cb21f8ff10eb6120f7153
Carlos Cuba <carlos.cuba@bonzzu.com>
2017-08-14 00:15:52 -0500

    Assets added. Background and logo image added. Currency text field element created and class added to override it's initialization with custom properties.

---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,27 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Carlos Cuba on 8/14/17.
+//  Copyright © 2017 Carlos Cuba. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2468635949)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
